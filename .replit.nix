c6e8b80a1d7e50ed8ed68461bf973f9700628122
Prateek Raj <prateek.raj3711@gmail.com>
2025-10-30 23:01:23 +0530

    Update .replit.nix

--- .replit.nix
+++ .replit.nix
@@ -3,6 +3,29 @@
     pkgs.nodejs-18_x
     pkgs.nodePackages.npm
     pkgs.nodePackages.typescript-language-server
+
+    # Playwright runtime OS deps
+    pkgs.chromium
+    pkgs.glib
+    pkgs.nspr
+    pkgs.nss
+    pkgs.dbus
+    pkgs.at-spi2-core
+    pkgs.cups
+    pkgs.gtk3
+    pkgs.pango
+    pkgs.cairo
+    pkgs.libdrm
+    pkgs.mesa
+    pkgs.alsaLib
+    pkgs.libxkbcommon
+    pkgs.xorg.libX11
+    pkgs.xorg.libXcomposite
+    pkgs.xorg.libXdamage
+    pkgs.xorg.libXext
+    pkgs.xorg.libXfixes
+    pkgs.xorg.libXrandr
+    pkgs.xorg.libxcb
   ];
 }
 
